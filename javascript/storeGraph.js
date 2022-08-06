125b19261906153ee812adf056b0ec8d77f0b6ad
Tunzeki <tundewebhead@gmail.com>
2022-08-06 17:28:09 +0100

    Store a store
    Store a graph an an edge list, an adjacency matrix,
    and an adjacency list

---
+++ javascript/storeGraph.js
@@ -0,0 +1,23 @@
+var edgeList = [[0, 2], [1, 3], [2, 3], [2, 4], [3, 5], [4, 5]];
+
+/*
+ * Store an adjacency matrix
+ * We've stored a graph, with 6 vertices indexed 0-5, as an edge list in the variable edgeList.
+ * Store the same graph, as an adjacency matrix, in the variable adjMatrix
+ */
+
+var adjMatrix = [   [0, 0, 1, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 0],
+                    [0, 0, 0, 1, 1, 0],
+                    [0, 0, 0, 0, 0, 1],
+                    [0, 0, 0, 0, 0, 1],
+                    [0, 0, 0, 0, 0, 0]
+];
+
+/*
+ * Store an adjacency list
+ * Store the same graph, as an adjacency list, in the variable adjList.
+ */
+
+var adjList = [[2], [3], [3, 4], [5], [5], []
+];
