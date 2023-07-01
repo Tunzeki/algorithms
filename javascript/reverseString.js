6df929e99752974354b0ad125f5390b94fe06da2
Tunzeki <tundewebhead@gmail.com>
2023-07-01 09:57:05 +0100

    LeetCode's Reverse String Challenge
    Write a function that reverses a string.
    The input string is given as an array of characters s.
    You must do this by modifying the input array in-place with O(1) extra
    memory.

---
+++ javascript/reverseString.js
@@ -0,0 +1,17 @@
+/**
+ * Visit https://leetcode.com/problems/reverse-string/description/
+ * for full problem description
+ * 
+ * Write a function that reverses a string. 
+ * The input string is given as an array of characters s.
+ * You must do this by modifying the input array in-place with O(1) extra memory.
+ * 
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+    // Since this function takes an array as argument,
+    // there's a built-in function to reverse the elements of an array,
+    // so I'll just call that.
+    s.reverse();
+};
